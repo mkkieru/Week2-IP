30f73743c2767db74d2533a809a98d4aa6f85682
mkkieru <mark.kieru@student.moringaschool.com>
2021-04-11 22:05:12 +0300

    add variables

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var yourName = document.getElementById("fname").Value;
-
-function name() {
-    alert(yourName);
-}
+var CC = document.getElementById("century").value;
+var YY = document.getElementById("year").value;
+var MM = document.getElementById("month").value;
+var DD = document.getElementById("day").value;
+var male = document.getElementById("male");
+var female = document.getElementById("female");
