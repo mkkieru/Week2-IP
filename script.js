d56d0ef01a0e9915c16906f7cfd681047621a72a
mkkieru <mark.kieru@student.moringaschool.com>
2021-04-11 22:10:01 +0300

    add if statements

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ var female = document.getElementById("female");
 
 var Amale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var Afemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+if (MM <= 0 || MM > 12) {
+
+    alert("Please enter a valid month");
+
+} else if (DD <= 0 || DD > 31) {
+
+    alert("Please enter a valid day");
+
+} else {
+    var index = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+
+    var d = parseInt(index);
+}
